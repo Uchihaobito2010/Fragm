8c9554352f5db11107eb3b5025eeafc78a429002
Uchihaobito2010 <paraschourey08@gmail.com>
2025-12-31 18:49:20 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,10 @@
+# This is an alternative if the above doesn't work
+from fastapi import FastAPI
+import sys
+import os
 
+# Add the parent directory to the path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+# Import from main
+from main import app
